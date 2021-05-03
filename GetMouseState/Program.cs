d38aa8fbee963908124a3c2feb6706781b1fd6ec
onodera_sf <sf_yo_service_yellow@outlook.jp>
2021-05-03 10:09:32 +0900

    プロジェクト ファイルを追加します。

---
+++ GetMouseState/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GetMouseState
+{
+	public static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			using (var game = new Game1())
+				game.Run();
+		}
+	}
+}
